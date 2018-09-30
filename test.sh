e0d9130911dfefcca46351bd4b70e7bed2dc269e
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-09-30 18:53:50 +0800

    Remove stuff from test.sh to make faster

--- test.sh
+++ test.sh
@@ -13,8 +13,8 @@ mkdir -p $finished
 
 for i in ${1%/}/*; do
   echo sim "<" $i ">" "$out/$(basename $i)"
-  echo $(ls $1 | wc -l) remaining
-  echo $(ls $out | wc -l) finished
+  # echo $(ls $1 | wc -l) remaining
+  # echo $(ls $out | wc -l) finished
   (perf stat -e branch-misses,cache-misses,instructions,cycles,context-switches ./sim < "$i") > "$out/$(basename $i)" 2>&1
   mv $i $finished/
 done
